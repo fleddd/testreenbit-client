33390ecc8caf67cc17141e8e55057f52627fb585
fledd <fleddd>
2025-05-09 13:27:43 +0300

    gh-pages integration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "/testreenbit-client/",
 });
