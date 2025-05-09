f831f95e46bc8a55588a78d4678f6e5ce9da6915
fledd <fleddd>
2025-05-09 14:21:58 +0300

    routes fix

--- src/widgets/auth/ui/GoogleAuth/GoogleAuthButton.tsx
+++ src/widgets/auth/ui/GoogleAuth/GoogleAuthButton.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router";
 
 const GoogleAuthButton = () => {
+  const isProduction = true;
+  const url = isProduction
+    ? "https://testreenbit-server.onrender.com/api/auth/login"
+    : "http://localhost:3030/api/auth/login";
   return (
     <Link
-      to={"https://testreenbit-server.onrender.com/api/auth/login"}
+      to={url}
       className="px-2 py-1 rounded-md bg-blue-400 text-white cursor-pointer"
     >
       Login through Google
